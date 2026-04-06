9f77880f7497265a65c35cefff8510f197dc64b3
Durga Sai Prasad Ganapathi <ganapathisai548@gmail.com>
2026-04-06 15:55:24 +0530

    cookie updated

--- Controllers/WebControllers/AuthenticationController.js
+++ Controllers/WebControllers/AuthenticationController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const Login = async (req, res, next) => {
+    console.log("responding")
     res.cookie("token", "TestingCookie", {
         httpOnly: true,
         secure: true,
