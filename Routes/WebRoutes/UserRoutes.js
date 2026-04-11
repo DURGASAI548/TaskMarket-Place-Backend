94ac0ece5033b8d49f0a9b3afec19d2057031b4d
Durga Sai Prasad Ganapathi <ganapathisai548@gmail.com>
2026-04-11 15:24:57 +0530

    updated

--- Routes/WebRoutes/UserRoutes.js
+++ Routes/WebRoutes/UserRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../../Controllers/WebControllers/UserController")
 const VerifyToken = require('../../ExternalSources/JwtController');
-const {Upload} = require("../../ExternalSources/S3Controller")
+const {upload} = require("../../ExternalSources/S3Controller")
 // For Org adding
 router.get('/get-normal-users', VerifyToken,UserController.GetNormalUsers)
-router.post('/add-user',Upload.single("profile"),VerifyToken,UserController.AddUser);
+router.post('/add-user',upload.single("profile"),VerifyToken,UserController.AddUser);
 
 module.exports = router;
