1ff21abfdeb2489bb3f19785a918be82e4dc65fd
DURGASAI548 <ganapathisai548@gmail.com>
2026-05-07 21:29:17 +0530

    updated

--- Controllers/WebControllers/EvalutionPointController.js
+++ Controllers/WebControllers/EvalutionPointController.js
@@ -210,7 +210,68 @@ const UpdateEvaluationPoint = async (req, res) => {
   }
 };
 
+const DeleteEvaluationPoint = async (req, res) => {
+  try {
+    const userId = req.user?.id;
+    const { id } = req.params;
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Evaluation Point ID",
+      });
+    }
+
+    const user = await UserSchema.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (user.userType === "user") {
+      return res.status(403).json({
+        success: false,
+        message: "You are not allowed to delete evaluation points",
+      });
+    }
+
+    const point = await EvalutionPointSchema.findById(id);
+
+    if (!point) {
+      return res.status(404).json({
+        success: false,
+        message: "Evaluation point not found",
+      });
+    }
+
+    await EvalutionPointSchema.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Evaluation point deleted successfully",
+    });
+
+  } catch (error) {
+    console.error("Error in DeleteEvaluationPoint:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 
 exports.AddEvaluationPoint = AddEvaluationPoint
 exports.GetAllEvaluationPoints = GetAllEvaluationPoints
 exports.UpdateEvaluationPoint = UpdateEvaluationPoint
+exports.DeleteEvaluationPoint = DeleteEvaluationPoint
--- Routes/WebRoutes/EvalutionPointRoutes.js
+++ Routes/WebRoutes/EvalutionPointRoutes.js
@@ -7,5 +7,6 @@ const VerifyToken = require('../../ExternalSources/JwtController');
 router.post('/add-evalution-point',VerifyToken, EvalutionPointController.AddEvaluationPoint);
 router.get('/get-evalution-points',VerifyToken, EvalutionPointController.GetAllEvaluationPoints);
 router.post('/edit-evalution-point/:id',VerifyToken, EvalutionPointController.UpdateEvaluationPoint);
+router.delete('/delete-evalution-point/:id',VerifyToken, EvalutionPointController.DeleteEvaluationPoint);
 
 module.exports = router;
